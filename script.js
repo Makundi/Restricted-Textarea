009bdd2837a542e307ab371befcd5dd27fd3d1ec
kelvin <makundykelvin@gmail.com>
2025-11-11 11:09:02 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const textArea = document.getElementById("message");
+const counter = document.getElementById("counter");
+const MAX_LENGTH = 50;
+
+
+
+textArea.addEventListener("input", () => {
+
+    if (textArea.value.length > MAX_LENGTH) {
+        textArea.value = textArea.value.slice(0, MAX_LENGTH);
+    }
+
+    const currentLength = textArea.value.length;
+    counter.textContent = `${currentLength}/${MAX_LENGTH}`;
+
+    const isMax = currentLength === MAX_LENGTH;
+    const color = isMax ? "red" : "black";
+
+    textArea.style.borderColor = color;
+    counter.style.color = color;
+});
